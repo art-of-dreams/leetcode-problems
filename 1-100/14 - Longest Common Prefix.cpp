e853479b90770e13a1816d6a4bb5221fa8ec6dbb
Dan Vasilev <denis.vasilev@aura.com>
2022-08-24 00:12:53 +0300

    14

---
+++ 1-100/14 - Longest Common Prefix.cpp
@@ -0,0 +1,63 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+ 
+
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        unsigned size = unsigned(strs.size());
+
+        if (size == 0) {
+        	return "";
+        }
+
+        string current = strs[0];
+
+        for (unsigned i = 1; i < size; i++) {
+        	unsigned sameCount = 0;
+
+        	for (unsigned j = 0; j < unsigned(strs[i].size()); j++) {
+        		if (current[j] == strs[i][j]) {
+        			sameCount += 1;
+        		} else {
+        			break;
+        		}
+        	}
+
+        	current.resize(sameCount);
+
+        	if (current.empty()) {
+        		return "";
+        	}
+        }
+
+        return current;
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<string> v1 = {"flower","flow","flight"};
+    string res1 = s.longestCommonPrefix(v1);
+    cout << "flower, flow, flight -> " << res1 << endl;
+
+    vector<string> v2 = {"dog","racecar","car"};
+    string res2 = s.longestCommonPrefix(v2);
+    cout << "dog, racecar, car -> " << res2 << endl;
+}
