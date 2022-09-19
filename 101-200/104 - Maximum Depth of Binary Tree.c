f8a58c13dc2d487307e1056a5976d60a2f19ba94
Dan Vasilev <denis.vasilev@aura.com>
2022-09-19 23:42:24 +0300

    104 (2)

--- 101-200/104 - Maximum Depth of Binary Tree.c
+++ 101-200/104 - Maximum Depth of Binary Tree.c
@@ -24,6 +24,13 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+struct ListedListNode {
+    int depth;
+    struct TreeNode *tree_node;
+    struct ListedListNode *next;
+    struct ListedListNode *prev;
+};
+
 static int max(int x, int y) {
     return x > y ? x : y;
 }
@@ -37,6 +44,61 @@ int maxDepth(struct TreeNode* root) {
     int right = maxDepth(root->right);
 
     return max(left, right) + 1;
+}
+
+int maxDepth2(struct TreeNode* root) {
+    if (!root) {
+        return 0;
+    }
+
+    struct ListedListNode* dummy_node = (struct ListedListNode*)malloc(sizeof(struct ListedListNode));
+    dummy_node->depth = 0;
+    dummy_node->tree_node = NULL;
+    dummy_node->next = NULL;
+    dummy_node->prev = NULL;
+
+    struct ListedListNode* tail = (struct ListedListNode*)malloc(sizeof(struct ListedListNode));
+    tail->depth = 1;
+    tail->tree_node = root;
+    tail->next = NULL;
+    tail->prev = dummy_node;
+
+    dummy_node->next = tail;
+
+    int res = 0;
+
+    while (dummy_node->next) {
+        int current_depth = tail->depth;
+        struct TreeNode* current_tree_node = tail->tree_node;
+        res = max(res, current_depth);
+
+        struct ListedListNode* prev = tail->prev;
+        prev->next = NULL;
+        free(tail);
+        tail = prev;
+
+        if (current_tree_node->left) {
+            struct ListedListNode* node = (struct ListedListNode*)malloc(sizeof(struct ListedListNode));
+            node->depth = current_depth + 1;
+            node->tree_node = current_tree_node->left;
+            node->next = NULL;
+            node->prev = tail;
+            tail->next = node;
+            tail = node;
+        }
+
+        if (current_tree_node->right) {
+            struct ListedListNode* node = (struct ListedListNode*)malloc(sizeof(struct ListedListNode));
+            node->depth = current_depth + 1;
+            node->tree_node = current_tree_node->right;
+            node->next = NULL;
+            node->prev = tail;
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    return res;
 }
 
 int main() {
