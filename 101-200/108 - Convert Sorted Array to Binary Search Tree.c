72480e1b87e54c5d23b0cbe9ec288fec1789c232
Dan Vasilev <denis.vasilev@aura.com>
2022-11-20 00:10:40 +0300

    108 (2)

--- 101-200/108 - Convert Sorted Array to Binary Search Tree.c
+++ 101-200/108 - Convert Sorted Array to Binary Search Tree.c
@@ -40,6 +40,26 @@ static struct TreeNode* helper(int* nums, int left, int right) {
     root->right = helper(nums, mid + 1, right);
     return root;
 }
+
+static struct TreeNode* helper2(int* nums, int left, int right) {
+    if (left > right) {
+        return NULL;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    // always choose right middle node as a root
+    if ((right - left) % 2 == 1) {
+        mid += 1;
+    }
+
+    // preorder traversal: node -> left -> right
+    struct TreeNode* root = malloc(sizeof(struct TreeNode));
+    root->val = nums[mid];
+    root->left = helper(nums, left, mid - 1);
+    root->right = helper(nums, mid + 1, right);
+    return root;
+}
  
 struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
     return helper(nums, 0, numsSize - 1);
