bb2a97951b226433b1abffa12b4b2e81c5a6df7e
Dan Vasilev <denis.vasilev@aura.com>
2022-11-24 17:30:55 +0300

    2119

---
+++ 2101-2200/2119 - A Number After a Double Reversal.c
@@ -0,0 +1,53 @@
+// Reversing an integer means to reverse all its digits.
+
+//     For example, reversing 2021 gives 1202.
+//     Reversing 12300 gives 321 as the leading zeros are not retained.
+
+// Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2.
+// Return true if reversed2 equals num. Otherwise return false.
+
+// Example 1:
+// Input: num = 526
+// Output: true
+// Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+
+// Example 2:
+// Input: num = 1800
+// Output: false
+// Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+
+// Example 3:
+// Input: num = 0
+// Output: true
+// Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
+
+// Constraints:
+//     0 <= num <= 10^6
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+static int reversed(int num) {
+    int res = 0;
+
+    while (num != 0) {
+        int pop = num % 10;
+        num /= 10;
+        res = res * 10 + pop;
+    }
+
+    return res;
+}
+
+bool isSameAfterReversals(int num) {
+    int first = reversed(num);
+    int second = reversed(first);
+    return num == second;
+}
+
+int main() {
+    printf("%d\n", reversed(526));
+    printf("%d\n", reversed(625));
+    printf("%d\n", isSameAfterReversals(526));
+}
